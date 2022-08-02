544ea18bfc5cbb63ce0688888d60e186244139e5
puzzud <puzzud@gmail.com>
2022-08-02 11:22:58 -0400

    Moved when and where a VIC raster line interrupt latching occurs.

--- A64Core/MOS6569.cpp
+++ A64Core/MOS6569.cpp
@@ -50,6 +50,11 @@ void CMOS6569::Cycle(){
 	if (++perLineClockCycle == NTSC_FIELD_CYCLES_PER_LINE){
 		perLineClockCycle = 0;
 
+		// TODO: Should this be called here or at a different point in time?
+		if (rasterLine == rasterLineCompare){
+			mRegs[0xD019 - 0xD000] |= 0x81; // Mark possible interrupt flags.
+		}
+
 		if (mRenderer != NULL){
 			mRenderer->OnRasterLineCompleted((unsigned int)rasterLine);
 		}
@@ -57,10 +62,6 @@ void CMOS6569::Cycle(){
 		if (++rasterLine == NTSC_FIELD_LINE_HEIGHT + 1){
 			rasterLine = 0;
 		}
-	}
-
-	if (rasterLine == rasterLineCompare){
-		mRegs[0xD019 - 0xD000] |= 0x81; // Mark possible interrupt flags.
 	}
 
 	bool rasterLineCompareIrqEnabled = (mRegs[0xD01A - 0xD000] & 0x01) != 0;
