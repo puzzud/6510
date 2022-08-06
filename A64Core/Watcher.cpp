4b2079b346c81523d30720d95ba7fd89fa0ca61a
puzzud <puzzud@gmail.com>
2022-08-06 19:16:36 -0400

    Implemented Watcher write watches.

--- A64Core/Watcher.cpp
+++ A64Core/Watcher.cpp
@@ -11,24 +11,45 @@
 
 CWatcher::CWatcher(){
     memset(mJumpWatches, 0, sizeof(unsigned int) * 0xFFFF);
+    memset(mReadWatches, 0, sizeof(unsigned int) * 0xFFFF);
+    memset(mWriteWatches, 0, sizeof(unsigned int) * 0xFFFF);
 }
 
 CWatcher::~CWatcher(){
 }
 
 void CWatcher::SetJumpWatch(u16 address){
-    mJumpWatches[address] = true;
+    mJumpWatches[address] = 1;
 }
 
 void CWatcher::ClearJumpWatch(u16 address){
-    mJumpWatches[address] = false;
+    mJumpWatches[address] = 0;
 }
 
-bool CWatcher::CheckJumpWatch(u16 address, eWatcherJumpType jumpType){
-    if (mJumpWatches[address]){
-        ReportJumpWatch(address, jumpType);
-        return true;
-    }
+bool CWatcher::CheckJumpWatch(u16 address){
+    return mJumpWatches[address] != 0;
+}
 
-    return false;
+void CWatcher::SetReadWatch(u16 address){
+    mReadWatches[address] = 1;
+}
+
+void CWatcher::ClearReadWatch(u16 address){
+    mReadWatches[address] = 0;
+}
+
+bool CWatcher::CheckReadWatch(u16 address){
+    return mReadWatches[address] != 0;
+}
+
+void CWatcher::SetWriteWatch(u16 address){
+    mWriteWatches[address] = 1;
+}
+
+void CWatcher::ClearWriteWatch(u16 address){
+    mWriteWatches[address] = 0;
+}
+
+bool CWatcher::CheckWriteWatch(u16 address){
+    return mWriteWatches[address] != 0;
 }
